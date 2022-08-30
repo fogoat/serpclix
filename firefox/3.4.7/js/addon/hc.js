b78a2af0a9c71bbbc308952f03b43e0136ba1d35
sunk818 <sunk818@users.noreply.github.com>
2022-08-30 14:34:15 -0700

    firefox serpclix 3.4.7

---
+++ firefox/3.4.7/js/addon/hc.js
@@ -0,0 +1,1 @@
+const serpclix_hcbox_vueapp=new Vue({el:"#serpclix_hcbox_app",render:hc_vueapp_compiled_template_render,staticRenderFns:hc_vueapp_compiled_template_static_render_funcs,data:{src:null},created:function(){browser.storage.local.get("auth_token").then((e=>{e.auth_token&&setTimeout((()=>{this.src=SERPCLIX_URLS.hc+e.auth_token}),1e3*(5+5*Math.random()))})).catch((e=>{}))},methods:{}});
